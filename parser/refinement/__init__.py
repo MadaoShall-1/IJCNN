c4ebc97876679e335ff0e5a4ac76d401bf041b53
Shuhuan YE <ye.shuhu@northeastern.edu>
2026-05-15 10:14:38 -0700

    Delete parser/refinement directory

--- parser/refinement/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Development-only Stage 0 refinement helpers.
-
-These modules analyze parser outputs and generate human-reviewable
-suggestions. They never modify parser rules at inference time.
-"""
-
